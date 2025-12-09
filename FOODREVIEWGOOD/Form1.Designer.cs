df39f59906f8e43dfd93dc297d4881f2b1194beb
OscarAllbut <255015@esher.ac.uk>
2025-12-09 15:08:14 +0000

    before period 5

---
+++ FOODREVIEWGOOD/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace FOODREVIEWGOOD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.checkedListBoxTags = new System.Windows.Forms.CheckedListBox();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.SignUpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.Title.Font = new System.Drawing.Font("Onyx", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Title.Location = new System.Drawing.Point(11, 10);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(1075, 96);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Food Review";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // checkedListBoxTags
+            // 
+            this.checkedListBoxTags.FormattingEnabled = true;
+            this.checkedListBoxTags.Location = new System.Drawing.Point(29, 159);
+            this.checkedListBoxTags.Name = "checkedListBoxTags";
+            this.checkedListBoxTags.Size = new System.Drawing.Size(238, 487);
+            this.checkedListBoxTags.TabIndex = 1;
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.BackColor = System.Drawing.Color.Olive;
+            this.UsernameLabel.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameLabel.Location = new System.Drawing.Point(363, 191);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(357, 50);
+            this.UsernameLabel.TabIndex = 2;
+            this.UsernameLabel.Text = "Username:";
+            this.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.BackColor = System.Drawing.Color.Olive;
+            this.PasswordLabel.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLabel.Location = new System.Drawing.Point(369, 326);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(357, 50);
+            this.PasswordLabel.TabIndex = 3;
+            this.PasswordLabel.Text = "Password:";
+            this.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameTextBox.Location = new System.Drawing.Point(336, 256);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(416, 55);
+            this.UsernameTextBox.TabIndex = 4;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTextBox.Location = new System.Drawing.Point(336, 396);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(416, 55);
+            this.PasswordTextBox.TabIndex = 5;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.LoginButton.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.LoginButton.Location = new System.Drawing.Point(336, 480);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(202, 62);
+            this.LoginButton.TabIndex = 6;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = false;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // SignUpButton
+            // 
+            this.SignUpButton.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.SignUpButton.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SignUpButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.SignUpButton.Location = new System.Drawing.Point(550, 480);
+            this.SignUpButton.Name = "SignUpButton";
+            this.SignUpButton.Size = new System.Drawing.Size(202, 62);
+            this.SignUpButton.TabIndex = 7;
+            this.SignUpButton.Text = "SignUp";
+            this.SignUpButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(1095, 702);
+            this.Controls.Add(this.SignUpButton);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.checkedListBoxTags);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.CheckedListBox checkedListBoxTags;
+        private System.Windows.Forms.Label UsernameLabel;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Button SignUpButton;
+    }
+}
+
---
+++ FOODREVIEWGOOD/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FOODREVIEWGOOD
+{
+    public partial class Form1 : Form
+    {
+        private const string CONNECT = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"M:\\Visual Studio 2022\\FOODREVIEWGOOD\\FoodReviewsDatabase.mdf\";Integrated Security=True;Connect Timeout=30";
+        private SqlConnection sqlConnection;
+        private bool LoggedIn;
+        private string Username;
+        private string Placeholde2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            sqlConnection = new SqlConnection(CONNECT);
+            GetTags();
+        }
+        
+        private void GetTags()
+        {
+            sqlConnection.Open();
+
+            //setup query
+            SqlCommand command = new SqlCommand("SELECT Tag FROM Tags ORDER BY Tag", sqlConnection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    checkedListBoxTags.Items.Add(reader["Tag"], false);
+                }
+            }
+            sqlConnection.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+
+            sqlConnection.Open();
+
+            //setup query
+
+            SqlCommand command = new SqlCommand("SELECT * FROM Users", sqlConnection);
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    if ((string)reader["UserName"] == UsernameTextBox.Text || (string)reader["UserEmail"] == UsernameTextBox.Text)
+                    {
+                        if ((string)reader["UserPassword"] == PasswordTextBox.Text)
+                        {
+                            LoggedIn = true;
+                            Username = UsernameTextBox.Text;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            sqlConnection.Close();
+
+            if (LoggedIn)
+
+            {
+                PasswordLabel.Hide();
+                UsernameLabel.Hide();
+                UsernameTextBox.Hide();
+                PasswordTextBox.Hide();
+                SignUpButton.Hide();
+                LoginButton.Hide();
+                //LOGIN FAIL LABLE
+            }
+
+            else
+            {
+                //SHOW LOGIN FAIL LABLE
+            }
+        }
+
+    }
+
+
+    
+}
